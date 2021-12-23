01385829e18ebbbb7b9f366e3b722de596adc167
Nguyen Cao Cuong <nguyencaocuong31082001@gmail.com>
2021-12-23 17:33:51 +0700

    registering the operations to the calculator

--- index.ts
+++ index.ts
@@ -1,6 +1,17 @@
+import Addition from "./Addition";
 import Calculator from "./Calculator";
+import Division from "./Division";
+import Mutiplication from "./Mutiplication";
+import Subtraction from "./Subtraction";
 
 const calculator = new Calculator();
+
+calculator
+  .register('+', new Addition())
+  .register('-', new Subtraction())
+  .register('x', new Mutiplication())
+  .register('/', new Division())
+;
 
 const [,,number1, operation, number2] = process.argv;
 const formattedNumber1 = parseInt(number1);
