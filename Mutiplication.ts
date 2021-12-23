b4eb7dbeb9dc04d9f43ec29a181da6506be72d57
BuiThanhLong <lbui21199@gmail.com>
2021-12-23 16:48:31 +0700

    add mutiplication

---
+++ Mutiplication.ts
@@ -0,0 +1,8 @@
+import { Operation } from "./Operation";
+
+class Mutiplication implements Operation {
+    run(number1: number, number2: number): number {
+        return number1*number2
+    } 
+    
+}
