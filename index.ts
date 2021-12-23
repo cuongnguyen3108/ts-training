ead8d67d8508f65523fa0334e70a2be742e93690
Nguyen Cao Cuong <nguyencaocuong31082001@gmail.com>
2021-12-23 15:40:00 +0700

    calculator

--- index.ts
+++ index.ts
@@ -1,70 +1,30 @@
+import Calculator from "./Calculator";
 
-class Human {
-  name: string
-  weight: number
 
-  constructor(name: string, weight: number) {
-    this.name = name;
-    this.weight = weight;
-  }
+let calculator = new Calculator();
+
+
+let [,,number1, operation, number2] = process.argv;
+let formattedNumber1 = parseInt(number1);
+let formattedNumber2 = parseInt(number2);
+let result = NaN;
+
+switch(operation) {
+  case '+': 
+    result = calculator.add(formattedNumber1, formattedNumber2)
+    break;
+
+  case '-':
+    result = calculator.subtract(formattedNumber1, formattedNumber2)
+    break;
+  case '*':
+    result = calculator.mutiply(formattedNumber1, formattedNumber2)
+    break;
+  case '/':
+    result = calculator.divide(formattedNumber1, formattedNumber2)
+    break;
+  default:
+    throw new Error(`Operation [${operation}] is not supported`);
 }
 
-class Cat {
-  name: string
-  weight: number
-
-  constructor(name: string, weight: number) {
-    this.name = name;
-    this.weight = weight;
-  }
-
-}
-
-
-let me = new Human('Rikky', 75);
-let myPet = new Cat('Kitty', 1.4);
-
-
-class Coordinator {
-  x: number = 0;
-  y: number = 0;
-
-  constructor(x: number, y: number) {
-    this.x = x;
-    this.y = y;
-  }
-
-  distance(other: Coordinator) {
-    return Math.sqrt(
-      Math.pow(this.x - other.x, 2) +
-      Math.pow(this.y - other.y, 2)
-    )
-  }
-}
-
-class Fraction {
-
-  x: number;
-  y: number;
-
-  constructor(x: number, y: number) {
-    this.x = x;
-    this.y = y;
-  }
-  sum(other: Fraction) {
-    return ((this.x*other.y)+(other.x*this.y))/(this.y*other.y)
-  }
-  divisionCalculation(other: Fraction) {
-    return (this.x*other.y)/(this.y*other.x)
-  }
-}
-
-
-const m = new Coordinator(3, 4);
-const n = new Coordinator(1, 6);
-
-const x = new Fraction(1,2);
-const a = new Fraction(4,5);
-
-console.log(x.sum(a));
-console.log(x.divisionCalculation(a));
+console.log(result);
