11e1f6a5906c35a50cae44a102fd1eecc00d6d5b
Quan le <quanleex113@gmail.com>
2021-12-27 17:47:20 +0700

    providing server file

---
+++ src/server.ts
@@ -0,0 +1,28 @@
+import express from 'express';
+import Calculator from "./Calculator";
+import supportedOperations from "./config";
+
+const calculator = new Calculator();
+
+supportedOperations.forEach(([operationName, operation]) => {
+  calculator.register(operationName, operation)
+})
+
+
+const app = express();
+const router = express.Router();
+
+router.get('/calculate', (request, response) => {
+    
+    const no1 = parseInt(request.query.no1 as string);
+    const no2 = parseInt(request.query.no2 as string);
+
+
+    return response.json({
+        result: calculator.calculate(request.query.operation as string, no1, no2)
+    })
+});
+
+app.use(router);
+
+app.listen(8080);
