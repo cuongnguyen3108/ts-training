17a88e0bf41e09a49b16de7a05550c9e4c633f34
Viet Linh <vietlinhdt2001@gmail.com>
2021-12-29 12:04:06 +0700

    promise

--- src/index.ts
+++ src/index.ts
@@ -12,3 +12,17 @@ const formattedNumber1 = parseInt(number1);
 const formattedNumber2 = parseInt(number2);
 
 console.log(calculator.calculate(operation, formattedNumber1, formattedNumber2));
+
+
+
+const wait = (ms) => {
+  return new Promise((resolve, reject)=>{
+    setTimeout(()=>{
+      resolve()
+    },1000)
+  });
+}
+wait(1000).then(()=>{
+  console.log("hello anh linh")
+})
+
